420e4faa914b551489d8078744fca1f95f2de66b
cmas1 <car.masone@gmail.com>
2023-03-03 11:09:02 +0100

    Add files via upload
    Aggiunto scheletro esercitazione

---
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -0,0 +1,50 @@
+package it.polito.tdp.Lab01;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class FXMLController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnInserisci;
+
+    @FXML
+    private Button btnReset;
+
+    @FXML
+    private TextField txtParola;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    void doInsert(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doReset(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+
+    }
+
+}
